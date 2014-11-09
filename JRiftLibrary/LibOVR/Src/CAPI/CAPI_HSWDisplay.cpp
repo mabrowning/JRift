4be536667d2a57f6627ae0fe6146fae95484993d
Zach Jaggi <feilen1000@gmail.com>
2014-11-09 03:51:17 -0600

    Improve/fix linux native build system

--- JRiftLibrary/LibOVR/Src/CAPI/CAPI_HSWDisplay.cpp
+++ JRiftLibrary/LibOVR/Src/CAPI/CAPI_HSWDisplay.cpp
@@ -279,7 +279,7 @@ bool HSWDisplay::TickState(ovrHSWDisplayState *hswDisplayState, bool graphicsCon
             {
                 if(IsDisplayViewable()) // If the HMD is mounted and otherwise being viewed by the user...
                 {
-                    //Display(); // Disable for now!
+                    Display();
                     newlyDisplayed = true;
                 }
             }
