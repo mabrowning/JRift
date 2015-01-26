7921f79fe7dfc8e1e713d960e42852b517c286e0
StellaArtois <pete.tabram@gmail.com>
2015-01-26 08:21:15 +0000

    Add rudimentary OS X build script

---
+++ JRiftLibrary/build_osx.sh
@@ -0,0 +1,27 @@
+SCRIPTDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo $SCRIPTDIR
+CURDIR=.
+echo $CURDIR
+
+rm -rf "$SCRIPTDIR/natives/osx/libJRiftLibrary64.jnilib"
+rm -rf "$SCRIPTDIR/natives/osx/libJRiftLibrary64.dylib"
+rm -rf "$SCRIPTDIR/osx64"
+mkdir "$SCRIPTDIR/osx64"
+cd "$SCRIPTDIR/osx64"
+cmake -DCMAKE_OSX_ARCHITECTURES=x86_64 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 "$SCRIPTDIR" -Wno-dev
+make
+cp ./libJRiftLibrary64.dylib "$SCRIPTDIR/natives/osx/libJRiftLibrary64.jnilib"
+cd "$CURDIR"
+
+rm -rf "$SCRIPTDIR/natives/osx/libJRiftLibrary.jnilib"
+rm -rf "$SCRIPTDIR/natives/osx/libJRiftLibrary.dylib"
+rm -rf "$SCRIPTDIR/osx32"
+mkdir "$SCRIPTDIR/osx32"
+cd "$SCRIPTDIR/osx32"
+cmake -DCMAKE_OSX_ARCHITECTURES=i386 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.7 "$SCRIPTDIR" -Wno-dev
+make
+cp ./libJRiftLibrary.dylib "$SCRIPTDIR/natives/osx/libJRiftLibrary.jnilib"
+cd "$CURDIR"
+
+
+
